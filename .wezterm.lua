ff4e2d67076c067408ecd505e1509688d393b6fb
Eduardo Herrera-Barraza <eduardoherrerabarra@gmail.com>
2026-07-07 17:47:09 -0400

    changed the font to Iosevka

--- .wezterm.lua
+++ .wezterm.lua
@@ -18,8 +18,8 @@ config.colors = {
 	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
 }
 
-config.font = wezterm.font("JetBrains Mono")
-config.font_size = 20.00
+config.font = wezterm.font("Iosevka Nerd Font")
+config.font_size = 18.00
 
 config.enable_tab_bar = false
 
